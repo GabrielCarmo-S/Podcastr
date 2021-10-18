7fa85b50fe9dd23c0f7d30d35024ac135feb90af
GabrielCarmo-S <perien.op@gmail.com>
2021-10-18 15:48:40 -0300

    Atualizaçao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,49 @@
 import React from "react";
+import {format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export default function Home() {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+import { GetStaticProps } from 'next';
+import { api } from "../services/api";
+
+export default function Home(props: HomeProps) {
+
   return (
     <h1></h1>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.url,
+    };
+  })
+;
   return {
     props: {
       episodes: data,
